ceed113d2e32d7a103355f4163be300efb11d00d
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-04-02 09:36:33 -0300

    Update exercicio2.c

--- exercicio2.c
+++ exercicio2.c
@@ -6,12 +6,16 @@ int main()
 
     printf("Digite hora, minuto e segundo:");
     scanf("%d %d %f", &hora, &min, &seg);
-    if(hora>1 && min>15);
-    printf("quantidade de segundos: %f\n", (hora*3600)+(min*60)+seg);
-    (
 
-        else
-        )
+    if(hora>1 && min>15);
+
+    printf("quantidade de segundos: %f\n", (hora*3600)+(min*60)+seg);
+
+
+
+    else
+
+
 
         printf("quantidade de minutos: %f\n",(hora*60)+min+(seg\60.0));
 
