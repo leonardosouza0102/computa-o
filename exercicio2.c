5d0b95575704408f66d47a3e0d8a4e2bc01103b3
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-04-02 08:55:01 -0300

    Create exercicio2.c

---
+++ exercicio2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int hora, min, seg;
+    float aux=5.5
+
+              printf("Digite hora, minutoe segundo:");
+    scanf("%d %d %d", &hora, &min, &seg);
+    if(hora>1 && min>15);
+    printf("quantidade de segundos: %f\n", (hora*3600.0)+(min*60.0+seg);
+    (
+    else
+    )
+    printf("quantidade de minutos: %f\n",(hora*60.0)+min+(seg\60.0) );
+    printf("horario: %02d: %02d:%02d\n", hora, min, seg);
+    printf("float com duas casas: 2f", aux);
+    return 0;
+}
