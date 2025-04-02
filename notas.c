cd9878850f6df676e58e00c5646a1b6e3009f330
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-04-02 10:35:55 -0300

    Update notas.c

--- notas.c
+++ notas.c
@@ -3,7 +3,7 @@ int main()
 {
   float n1, n2, n3,
         media;
-printf("Digite as tres notas:"):
+printf("Digite as tres notas:");
 scanf("%f %f %f", &n1, &n2,&n3);
 media=(n1+n2+n3)/3;
 if(media>8)
@@ -13,6 +13,6 @@ else if(media>=6)
 else if(media>=4)
   printf("Vai para Final");
 else if(media<4)
-  printf("Reprovado Direto"):
+  printf("Reprovado Direto");
 return 0;
 }
