245a51b8312c7949f3e18f91cca6095ab9dc7dcb
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-04-02 10:32:16 -0300

    Update notas.c

--- notas.c
+++ notas.c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+int main()
+{
+  float n1, n2, n3,
+        media;
+printf("Digite as tres notas:"):
+scanf("%f %f %f", &n1, &n2,&n3);
+media=(n1+n2+n3)/3;
+if(media>8)
+  printf("Aprovado com Distinção");
+else if(media>=6)
+  printf("Aprovado Direto");
+else if(media>=4)
+  printf("Vai para Final");
+else if(media<4)
+  printf("Reprovado Direto"):
+return 0;
+}
