ce9754d4832da2e0238312fadd876d103b1ed1e8
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-04-02 09:54:04 -0300

    Update exercicio2.c

--- exercicio2.c
+++ exercicio2.c
@@ -8,28 +8,12 @@ int main()
     scanf("%d %d %f", &hora, &min, &seg);
 
     if(hora>1 && min>15)
-
-
     {
         printf("quantidade de segundos: %f\n", (hora*3600)+(min*60)+seg);
     }
-
-
-
-
     else
-
-
-
-
     {
         printf("quantidade de minutos: %f\n",(hora*60)+min+(seg/60.0));
     }
-
-
-
-
-
-
     return 0;
 }
