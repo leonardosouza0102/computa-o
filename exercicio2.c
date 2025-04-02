e7439f821ab2eb45bac2f9ef7013088bac796d38
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-04-02 09:01:33 -0300

    Update exercicio2.c

--- exercicio2.c
+++ exercicio2.c
@@ -10,7 +10,7 @@ int main()
     (
     else
     )
-    printf("quantidade de minutos: %f\n",(hora*60.0)+min+(seg\60.0) );
+    printf("quantidade de minutos: %f\n",(hora*60.0)+min+(seg\60.0));
     printf("horario: %02d: %02d:%02d\n", hora, min, seg);
     printf("float com duas casas: 2f", aux);
     return 0;
