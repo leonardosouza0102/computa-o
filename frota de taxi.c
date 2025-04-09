0d59f2a8c6dc1a1698ef98a3f47c041c5efa5015
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-04-09 09:43:05 -0300

    Create frota de taxi.c

---
+++ frota de taxi.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+float n1, PreçodoÁlcool;
+printf("Preço do Álcool:");
+scan("%f", &n1);
+float n2, PreçodaGasolina;
+printf("Preço da Gasolina:");
+scanf("%f", &n2);
+float n4, n5, CustodoAĺcool;
+printf("km e L:");
+scanf("%f %f ", &n4, &n5);
+CustodoÁlcool=(n4/n5)*n1
+float n6, n7, CustodaGasolina;
+printf("km e L:");
+scanf("%f %f", &n6, %n7);
+CustodaGasolina=(n6/n7)*n2:
+  if(CustodoÁlcool>CustodaGasolina)
+  print("Use Gasolina");
+else if(CustodoÁlcool<CustodaGasolina)
+  printf("Use Álcool");
+  else
+  printf("Use Gasolina");
+return 0;
+}
