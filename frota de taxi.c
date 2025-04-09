3b62b5924920999e4e7e778029e2c8f3ab78a0fa
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-04-09 09:48:30 -0300

    Update frota de taxi.c

--- frota de taxi.c
+++ frota de taxi.c
@@ -3,7 +3,7 @@ int main()
 {
 float n1, PreçodoÁlcool;
 printf("Preço do Álcool:");
-scan("%f", &n1);
+scanf("%f", &n1);
 float n2, PreçodaGasolina;
 printf("Preço da Gasolina:");
 scanf("%f", &n2);
@@ -16,7 +16,7 @@ printf("km e L:");
 scanf("%f %f", &n6, &n7);
 CustodaGasolina=(n6/n7)*n2;
   if(CustodoÁlcool>CustodaGasolina)
-  print("Use Gasolina");
+  printf("Use Gasolina");
 else if(CustodoÁlcool<CustodaGasolina)
   printf("Use Álcool");
   else
