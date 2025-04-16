02695b5b8da2b9e7e609703b3ab25b25cf7b85de
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-04-16 09:26:37 -0300

    Update exercício while indet.1.c

--- exercício while indet.1.c
+++ exercício while indet.1.c
@@ -1,15 +1,1 @@
-#include <stdio.h>
-int main()
-{
-    int i=1,n;
-    printf("Digite um Número:");
-    scanf("%d", &n);
-        while(n>=0)
-{
-      printf("%d", n);
-      i++
-}
-printf("%d", n);
-return 0;
-}
-    
+
