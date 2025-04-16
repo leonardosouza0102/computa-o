3a6a8b8207ea59c6c5120b13dcdd899a36f1ffeb
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-04-16 09:20:26 -0300

    Update i.c

--- i.c
+++ i.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 int main()
 {
-int i=0;
-while(i<=100)
-  {
-  if(i%13==2)
-  {
-    printf("%d", i)
-    }
-  i++
-    }
+    int i=1,n;
+    printf("Digite um Número:");
+    scanf("%d", &n);
+        while(n>=0)
+{
+      printf("%d", n);
+      i++
+}
+printf("%d", n);
 return 0;
 }
+    
