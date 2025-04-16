ca322e76c4e7255f67ecd730eacfc18e912c7cce
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-04-16 09:13:56 -0300

    exercício while ind.1.c

---
+++ exercício for.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i=1, n
+    printf("Digite um número:");
+    scanf("%d",&n);
+          while(n>=0)
+    {
+        printf("%d", i);
+        i++
+    }
+    printf("%d", n);
+    return 0;
+}
