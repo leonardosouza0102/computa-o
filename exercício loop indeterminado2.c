9d0270be3ef259dc172e6cb302f0815ac9f581b6
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-04-16 09:28:44 -0300

    eexercícioloop1.c

---
+++ exercício loop indeterminado2.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+int main()
+{
+    int i=1, n, media;
+    printf("Digite um número:");
+    scanf("%d",&n);
+          while(n!=(-1000))
+    {
+        printf("%d", );
+        i++
+    }
+    if(n>0)
+    {
+    printf("%d", n);
+    media=n/
+    printf("%d", media);
+    }
+    return 0;
+}
