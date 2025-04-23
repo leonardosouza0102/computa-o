4e95bf03eaad64f99e01a59baa383b2a9e55472e
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-04-23 08:24:14 -0300

    Add files via upload

---
+++ tinta.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <stdio.h>
+int main()
+{
+    float altura, largura, area, litro;
+    int LITRO;
+    printf("Digite a altura e alargura da parede em metros: ");
+    scanf("%f %f", &altura, &largura);
+    area=altura*largura;
+    litro=area/2;
+    LITRO=ceil(litro);
+    printf("Área da parede em metros quadrados: %f\n", area);
+    printf("Quantidade de tinta em litros: %d", LITRO);
+
+    return 0;
+}
