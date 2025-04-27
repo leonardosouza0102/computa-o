2dae3e34054ecfc6ec0300c119c1f3989e34ca85
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-04-27 11:53:53 -0300

    Create Resto dois de 1 até 100.c

---
+++ Resto dois de 1 até 100.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n, i=1, x;
+    printf("Digite um número\n");
+    scanf("%d", &n);
+    x=n+2;
+    while(i<=100)
+    {
+        x%n==2;
+        printf("%d\n", x);
+        x+=n;
+        i++;
+    }
+    
+    return 0;
+}
