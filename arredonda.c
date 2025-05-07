46b241eaa00ddfabbb7ce9f2ec03523b3815a826
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-05-07 09:36:08 -0300

    Create arredonda.c

---
+++ arredonda.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+float arredonda(float x, float y)
+{
+    if(y>=0)
+    return x=round(y);
+    else
+    return x=round(y*(-1));
+}
+int main()
+{
+    float valor, arredondado;
+    printf("Digite um valor: ");
+    scanf("%f", &valor);
+    if(valor>=0)
+    printf("O valor arredondado eh: %.1f", arredonda(arredondado, valor));
+    else
+    {
+    printf("O valor arredondado eh: -%.1f", arredonda(arredondado, valor));
+    }
+}
