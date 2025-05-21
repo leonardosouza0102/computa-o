c2e25c95e013df012cf497d571c1729410b6a672
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-05-21 10:12:19 -0300

    Update erros_matriz.c

--- erros_matriz.c
+++ erros_matriz.c
@@ -40,20 +40,19 @@ void matriz(int *tam1, int *tam2, int m[*tam1][*tam2], int *y, int *x)
     *tam1=rand()%11;
     *tam2=rand()%11;
     for(i=0; i<*tam1; i++)
-    {
-        *y=rand()%101;
+     {
         for(j=0; j<*tam2; j++)
-       {
+        {
+        *y=rand()%101;
         *x=rand()%101;
          printf("%d\n", m[*y][*x]);
-       }
-        
-    }
+        { 
+     }
 }
 int main()
 {
     int tam1, tam2;
-    int m[tam1][tam2], y, x, i, j;
+    int m[tam1][tam2], y, x;
     matriz(&tam1, &tam2, m[tam1][tam2], &y, &x);
     return 0;
 }
