9cee81b133be845ef86d455694d65d0421b84ced
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-05-28 09:59:57 -0300

    Update bingo.c

--- bingo.c
+++ bingo.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 #define N 5
-void preencheBingo(int m[][N], int i, int j)
+void preencheBingo(int m[N][N], int i, int j)
 {
     srand(time(NULL));
     for(i=0; i<N; i++)
       for(j=0; j<N; j++)
           m[i][j]=rand()%100;
 }
-void imprime( int m[][N], int i, int j)
+void imprime( int m[N][N], int i, int j)
 {
      for(i=0; i<N; i++)
      {
         for(j=0; j<N; j++)
         {
-            if(m[i][j]==m[i][j])
+            if(m[i][j]!=m[i][j])
               printf("%d ", m[i][j]);
             else
               break;
