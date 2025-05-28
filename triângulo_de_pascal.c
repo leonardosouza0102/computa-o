f6589daa91409a3483ef0d40ade528e8a82b6d72
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-05-28 19:48:42 -0300

    Update and rename triângulo_de_pascal_errado.c to triângulo_de_pascal.c

---
+++ triângulo_de_pascal.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+   int x, i, j;
+   printf("Digite um número: ");
+   scanf("%d", &x);
+   int m[x][x];
+   for(i=0; i<x; i++)
+   {
+       for(j=0; j<=i; j++)
+       {
+            if(j==0 || j==i)
+            {
+              m[i][j]=1;
+            }
+            else
+            {
+              //m[i+1][j+1]=m[i][j]+m[i][j+1];
+              m[i][j] = m[i-1][j-1] + m[i-1][j];
+              printf("%d ", m[i][j]);
+            }
+       }
+      printf("\n");
+   }
+   return 0;
+}
