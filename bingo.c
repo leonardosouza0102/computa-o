c781afb87ea77bc246078de45a87ad47be554077
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-05-28 08:52:51 -0300

    Update bingo.c

--- bingo.c
+++ bingo.c
@@ -3,19 +3,29 @@
 void preencheBingo(int m[][N], int i, int j)
 {
     srand(time(NULL));
-  for(i=0; i<N; i++)
-  {
+    for(i=0; i<N; i++)
       for(j=0; j<N; j++)
-      {
           m[i][j]=rand()%100;
-          printf("%d ", m[i][j]);
-      }
-      printf("\n");
-  }      
 }
+void imprime( int m[][N], int i, int j)
+{
+     for(i=0; i<N; i++)
+     {
+        for(j=0; j<N; j++)
+        {
+            if(m[i][j]==m[i][j])
+              printf("%d ", m[i][j]);
+            else
+              break;
+        }
+     printf("\n"); 
+    }
+    
+}  
 int main()
 {
     int m[5][5], i=0, j=0;
     preencheBingo(m, N, N);
+    imprime(m, N, N);
     return 0;
 }
