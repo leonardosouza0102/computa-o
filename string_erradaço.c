68a795a9dd55243821506a777f2c4a2d36e8504c
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-05-29 19:59:03 -0300

    Update string_erradaço.c

--- string_erradaço.c
+++ string_erradaço.c
@@ -2,7 +2,7 @@
 #define T 100
 int main()
 {
-   int j, contaa, contae, contai, contao, contau;
+   int j, xa, xe, xi, xo, xu;
    char string[T];
    char a='a';
    char e='e';
@@ -14,25 +14,60 @@ int main()
    for(j=0; string[j]; j++)
    {
        if(string[j] == a)
-       {
-           contaa++;
-           printf("a:");
-           for(j=1; string[j]; j++)
-           {
-               printf("*");
-           }
-           printf("(%d)\n", contaa);
-       }
+         xa++;
        else if(string[j] == e)
-       {
-           printf("e:");
-           for(j=1; string[j]; j++)
-           {
-               j++;
-               printf("*");
-           }
-           printf("(%d)\n", j);
-       }
+         xe++;
+       else if(string[j] == i)
+         xi++;
+       else if(string[j] == o)
+         xo++;
+       else if(string[j] == u)
+         xu++;
    }
-   return 0;
+   if(string[j] == a)
+   {
+       printf("a:");
+       for(j=1; j<=xa; j++)
+       {
+           printf("*");
+       }
+       printf("(%d)\n", xa);  
+   }
+   else if(string[j] == e)
+   {
+       printf("e:");
+       for(j=1; j<=xe; j++)
+       {
+           printf("*");
+       }
+       printf("(%d)\n", xe);  
+   }
+   else if(string[j] == i)
+   {
+       printf("i:");
+       for(j=1; j<=xi; j++)
+       {
+           printf("*");
+       }
+       printf("(%d)\n", xi);  
+   }
+    else if(string[j] == o)
+   {
+       printf("o:");
+       for(j=1; j<=xo; j++)
+       {
+           printf("*");
+       }
+       printf("(%d)\n", xo);  
+   }
+   else if(string[j] == u)
+   {
+       printf("u:");
+       for(j=1; j<=xu; j++)
+       {
+           printf("*");
+       }
+       printf("(%d)\n", xu);  
+   }
+    return 0;
 }
