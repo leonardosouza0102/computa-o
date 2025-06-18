d2b4923f6bbaef2cad636c6026ec313e9bfed982
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-06-18 10:44:17 -0300

    Update FILE_erradaço.c

--- FILE_erradaço.c
+++ FILE_erradaço.c
@@ -22,3 +22,26 @@ int main()
     fclose(exercicio2);
     return 0;
 }
+
+//#include<stdio.h>
+#define  T 100
+int main()
+{
+    char n[T];
+    float soma=0, valor;
+    FILE *exercicio2;
+    exercicio2 = fopen("teste2.txt", "r");
+    
+    if(exercicio2 == NULL)
+    {
+        printf("O arquivo não foi encontrado");
+        return 1;
+    }
+    while(fscanf(exercicio2, "%f", &valor)==1)
+    {
+        soma==valor++;
+    }
+    printf("%f", soma);
+    fclose(exercicio2);
+    return 0;
+}
