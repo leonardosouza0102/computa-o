0af3fb56e7907012484161a120737df5af4e8182
leonardosouza0102 <leonardo.010207@alunos.utfpr.edu.br>
2025-06-18 10:37:54 -0300

    Create FILE_erradaço.c

---
+++ FILE_erradaço.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define  T 4
+int main()
+{
+    char n[T];
+    float soma=0, valor;
+    FILE *exercicio2;
+    exercicio2 = fopen("teste2.txt", "r");
+    
+    if(exercicio2 == NULL)
+    {
+        printf("O arquivo não foi encontrado");
+        return 1;
+    }
+    while(fscanf(exercicio2, "%s", n)==0)
+    {
+        valor==atof(n);
+        soma==valor++;
+    }
+    printf("%f", soma);
+    fclose(exercicio2);
+    return 0;
+}
